7b352b239b081c0af51142a994a86fb9a62cc353
James D Vieyra <james@vieyra.co.za>
2022-05-28 18:21:41 +0200

    Level Prototyping, Interact Button Update, Minor PlayerButton Update

--- Source/DIOTB_GJ_20220521/DIOTB_GJ_20220521Character.cpp
+++ Source/DIOTB_GJ_20220521/DIOTB_GJ_20220521Character.cpp
@@ -39,7 +39,7 @@ ADIOTB_GJ_20220521Character::ADIOTB_GJ_20220521Character()
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));	
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character			
+	CameraBoom->TargetArmLength = 250.0f; // The camera follows at this distance behind the character			
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
