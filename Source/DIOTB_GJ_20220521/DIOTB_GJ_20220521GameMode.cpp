33a0f2d58118e743a42f230d70dd32130cb9d52d
James D Vieyra <james@vieyra.co.za>
2022-05-30 01:44:55 +0200

    Getting project ready for demo build

--- Source/DIOTB_GJ_20220521/DIOTB_GJ_20220521GameMode.cpp
+++ Source/DIOTB_GJ_20220521/DIOTB_GJ_20220521GameMode.cpp
@@ -7,10 +7,10 @@
 ADIOTB_GJ_20220521GameMode::ADIOTB_GJ_20220521GameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	}*/
 }
